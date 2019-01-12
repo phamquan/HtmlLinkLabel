62c37537668a1c50907a72732d4923a091321bd6
Quan Pham (VN) <quan.p@homecredit.vn>
2019-01-12 17:51:11 +0700

    * .gitignore: Init Project
    * HtmlLinkLabel.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * HtmlLinkLabel.csproj:
    * Entitlements.plist:
    * MainActivity.cs:
    * Default.png:
    * Default@2x.png:
    * HtmlLinkLabel.iOS.csproj:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * HtmlLabelConverter.cs:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * styles.xml:
    * Tabbar.axml:
    * AboutResources.txt:
    * HtmlLinkLabel.Android.csproj:
    * LaunchScreen.storyboard:
    * Toolbar.axml:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon76.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon80.png:
    * Icon180.png:
    * Icon120.png:
    * Icon167.png:
    * Icon152.png:
    * icon_round.xml:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ HtmlLinkLabel/Converters/HtmlLabelConverter.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace HtmlLinkLabel.Converters
+{
+    public class HtmlLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var formatted = new FormattedString();
+
+            foreach (var item in ProcessString((string)value))
+                formatted.Spans.Add(CreateSpan(item));
+
+            return formatted;
+        }
+
+        private Span CreateSpan(StringSection section)
+        {
+            var span = new Span()
+            {
+                Text = section.Text
+            };
+
+            if (!string.IsNullOrEmpty(section.Link))
+            {
+                span.GestureRecognizers.Add(new TapGestureRecognizer()
+                {
+                    Command = _navigationCommand,
+                    CommandParameter = section.Link
+                });
+                span.TextColor = Color.Blue;
+                // Underline coming soon from https://github.com/xamarin/Xamarin.Forms/pull/2221
+                // Currently available in Nightly builds if you wanted to try, it does work :)
+                // As of 2018-07-22. But not avail in 3.2.0-pre1.
+                // span.TextDecorations = TextDecorations.Underline;
+            }
+
+            return span;
+        }
+
+        public IList<StringSection> ProcessString(string rawText)
+        {
+            const string spanPattern = @"(<a.*?>.*?</a>)";
+
+            var sections = new List<StringSection>();
+
+            if (String.IsNullOrEmpty(rawText)) return sections;
+
+            MatchCollection collection = Regex.Matches(rawText, spanPattern, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+            var lastIndex = 0;
+
+            foreach (Match item in collection)
+            {
+                var foundText = item.Value;
+                sections.Add(new StringSection() { Text = rawText.Substring(lastIndex, item.Index - lastIndex) });
+                lastIndex = item.Index + item.Length;
+
+                // Get HTML href 
+                var html = new StringSection()
+                {
+                    Link = Regex.Match(item.Value, @"(?<=href=(\""|\“))[\S]+(?=(\""|\”))", RegexOptions.IgnoreCase).Value,
+                    Text = Regex.Replace(item.Value, "<.*?>", string.Empty)
+                };
+
+                sections.Add(html);
+            }
+
+            sections.Add(new StringSection() { Text = rawText.Substring(lastIndex) });
+
+            return sections;
+        }
+
+        public class StringSection
+        {
+            public string Text { get; set; }
+            public string Link { get; set; }
+        }
+
+        private ICommand _navigationCommand = new Command<string>((url) =>
+        {
+            Console.WriteLine($"Open for {url}");
+            try
+            {
+                Device.OpenUri(new Uri(url));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        });
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HtmlLinkLabel/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HtmlLinkLabel
+{
+    public partial class MainPage : ContentPage
+    {
+        String _Html;
+        public String Html
+        {
+            get
+            {
+                return _Html;
+            }
+            set
+            {
+                _Html = value;
+                OnPropertyChanged(nameof(Html));
+            }
+        }
+
+        String _MessageContent = String.Empty;
+        public String MessageContent
+        {
+            get
+            {
+                return _MessageContent;
+            }
+            set
+            {
+                _MessageContent = value;
+                OnPropertyChanged(nameof(MessageContent));
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+            Html = @"<a href=""bizwork://profile/nghi"">@Nghi</a> xem cai nay di. <a href=""https://google.com"">google.com</a> MS: <a href=""http://microsoft.com"">Microsoft</a>.";
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Html = MessageContent;
+
+            var parseUrl = this.Urlize(MessageContent);
+            DisplayAlert("Hello", parseUrl, "OK");
+        }
+
+        string Urlize(String messageContent)
+        {
+            if (String.IsNullOrEmpty(messageContent)) return string.Empty;
+
+            var urlRegex = @"\b(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?(?!(\""|\”))\b";
+
+            MatchCollection collection = Regex.Matches(messageContent, urlRegex, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+            var result = String.Empty;
+
+            foreach(Match item in collection)
+            {
+                result += item.Value;
+            }
+
+            return result;
+        }
+    }
+}
